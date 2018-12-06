d952e4cbe9c203c2f6cd22e8968117916ebf1e32
aksingh75 <45658115+aksingh75@users.noreply.github.com>
2018-12-06 17:29:10 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(myDempProj.Startup))]
+namespace myDempProj
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
